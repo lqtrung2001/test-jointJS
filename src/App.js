913138ef2866b132bfbc7f89ef1d3cd3ef0e08bb
Luong Quoc Trung <lqtrung20122001@gmail.com>
2022-11-07 08:56:00 +0700

    generate shape

--- src/App.js
+++ src/App.js
@@ -4,138 +4,140 @@ import './App.css';
 import * as joint from 'jointjs'
 import svgPanZoom from 'svg-pan-zoom';
 import nextId from "react-id-generator";
+import data from './data.js'
+
+function makeLink(parentElement, childElement) {
+    return new joint.shapes.standard.Link({
+        source: { id: parentElement.id },
+        target: { id: childElement.id },
+    });
+}
+
+
+function checkCondition(text) {
+    var circleTemp = {
+        id: nextId(),
+        type: 'standard.Circle',
+        position: {
+            x: 100,
+            y: 100
+        },
+        size: {
+            width: 200,
+            height: 100
+        },
+        attrs: {
+            body: {
+                fill: 'red',
+            },
+            label: {
+                text: text,
+                fill: 'white'
+            }
+        }
+    }
+
+}
+
+var width = 100
+
+const makeCell = (shape, text) => {
+    width += 50;
+    return {
+        id: nextId(),
+        type: shape,
+        position: {
+            x: width,
+            y: 100
+        },
+        size: {
+            width: 200,
+            height: 100
+        },
+        attrs: {
+            body: {
+                fill: 'blue',
+            },
+            label: {
+                text,
+                fill: 'white'
+            }
+        }
+    }
+}
+
+function addToCells(jsonData, cells) {
+    jsonData.forEach(task => {
+        
+        if (task.dependencies) {
+            // console.log("caoooo", Object.keys(a.dependencies)[1] );
+            if (task.dependencies.type === "LogicalOperator") {
+                // console.log("collected: ", deepDive(task.dependencies.elements, "", cells))
+                deepDive(task.dependencies.elements, "", cells)
+                task.dependencies.id = nextId()
+                cells.push(makeCell("standard.Circle", task.dependencies.operator))
+            }
+        }
+        cells.push(makeCell("standard.Rectangle", task.id));
+    }
+    )
+    console.log(jsonData)
+}
+
+const deepDive = (node, parent, cells) => {
+    // const collector = []
+    // console.log("node ", node)
+    for (const key in node) {
+        console.log("loop to key ", node[key])
+        if (node[key].type === "LogicalOperator") {
+            // collector.push(deepDive(node[key].elements))
+            // TODO: Ve hinh tron AND/OR
+            console.log("Draw ", node[key])
+            node[key].id = nextId()
+            cells.push(makeCell("standard.Circle", node[key].operator))
+            // TODO: noi hinh tron voi parent
+
+            // Goi lai ham deepDive voi node = node[key].elements va parent = hinh tron
+        } else if (node[key].type === "Task") {
+            // console.log("task = ", node[key])
+            // collector.push(node[key])
+            // TODO: noi task voi parent cua no
+
+        }
+    }
+    // return collector;
+}
 
 function App() {
-    useEffect(() => {
-        function makeLink(parentElement, childElement) {
-            return new joint.shapes.standard.Link({
-                source: { id: parentElement.id },
-                target: { id: childElement.id },
-            });
-        }
-        var jsonExemple = '{"squadName": "Super hero squad","homeTown": "Metro City","formed": 206,"secretBase": "Super tower","active": true,"members": [{"name": "Molecule Man","age": 29,"secretIdentity": "Dan Jukes","powers": ["Radiation resistance","Turning tiny","Radiation blast"]}, {   "name": "Madame Uppercut","age": 39, "secretIdentity": "Jane Wilson", "powers": ["Million tonne punch", "Damage resistance",     "Superhuman reflexes"] },{"name": "Eternal Flame","age": 1000000,   "secretIdentity": "Unknown",      "powers": [        "Immortality",        "Heat Immunity","Inferno","Teleportation","Interdimensional travel"]} ]}'
-        var jsonData = JSON.parse(jsonExemple);
+    
+    const jsonData = data;
+    console.log(jsonData);
+    var namespace = joint.shapes;
+    var cells = []
+    addToCells(jsonData, cells)
+    var graph = new joint.dia.Graph({}, { cellNamespace: namespace });
+    graph.resetCells(cells);
 
-        var namespace = joint.shapes;
-        var cells = []
-        addToCells()
-        var graph = new joint.dia.Graph({}, { cellNamespace: namespace });
-        graph.resetCells(cells);
+    useEffect(() => {
+
         var paper = new joint.dia.Paper({
             el: document.getElementById('myholder'),
             model: graph,
-            width: 1000,
-            height: 800,
-            gridSize: 1,
+            width: '100%',
+            height: '100%',
+            gridSize: 20,
             cellViewNamespace: namespace,
 
-        });
-
-        function addToCells() {
-            var height = 100;
-            var count = Object.keys(jsonData).length
-            console.log(count);
-            console.log(Object.keys(jsonData)[5]);
-            for (let i = 0; i < count; i++) {
-                var cell = {
-                    id: nextId(),
-                    type: 'standard.Rectangle',
-                    position: {
-                        x: 100,
-                        y: height
-                    },
-                    size: {
-                        width: 200,
-                        height: 100
-                    },
-                    attrs: {
-                        body: {
-                            fill: 'blue',
-                        },
-                        label: {
-                            text: Object.keys(jsonData)[i],
-                            fill: 'white'
-                        }
-                    }
-                }
-                if (cell.attrs.label.text == 'members') {
-                    var height2 = height;
-                    var countChild = Object.keys(jsonData.members).length
-                    for (let j = 0; j < countChild; j++) {
-                        var childcell = {
-                            id: nextId(),
-                            type: 'standard.Rectangle',
-                            position: {
-                                x: 400,
-                                y: height2
-                            },
-                            size: {
-                                width: 200,
-                                height: 100
-                            },
-                            attrs: {
-                                body: {
-                                    fill: 'red',
-                                },
-                                label: {
-                                    text: jsonData.members[j].name,
-                                    fill: 'white'
-                                }
-                            }
-                        }
-                        height2 += 150;
-                        cells.push(childcell);
-                        var link = makeLink(cell, childcell);
-                        cells.push(link);
-                    }
-                }
-                height += 150;
-                cells.push(cell);
-            }
-        }
-
-        const panZoom = svgPanZoom(paper.svg, {
-
-            viewportSelector: paper.layers,
-            zoomEnabled: true,
-            panEnabled: false,
-            controlIconsEnabled: true,
-            maxZoom: 2,
-            minZoom: 0.1,
-            onUpdatedCTM: (function () {
-                let currentMatrix = paper.matrix();
-                return function onUpdatedCTM(matrix) {
-                    const { a, d, e, f } = matrix;
-                    const { a: ca, d: cd, e: ce, f: cf } = currentMatrix;
-                    const translateChanged = (e !== ce || f !== cf)
-                    if (translateChanged) {
-                        paper.trigger('translate', e - ce, f - cf);
-                    }
-                    const scaleChanged = (a !== ca || d !== cd);
-                    if (scaleChanged) {
-                        paper.trigger('scale', a, d, e, f);
-                    }
-                    currentMatrix = matrix;
-                }
-            })()
-        });
-
-
-        paper.on('blank:pointerdown', function () {
-            panZoom.enablePan();
-        });
-
-        paper.on('blank:pointerup', function () {
-            panZoom.disablePan();
         });
     });
 
     return (
         <div id='myholder'>
             <h1>aaaaaaaaaa</h1>
-
+    
         </div>
     );
+
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,57 @@
+const data = [
+    {
+        "id": "B1",
+        "type": "B",
+        "dependencies":
+        {
+            "type": 'LogicalOperator',
+            "operator": "or",
+            "elements": [
+                {
+                    "type": "Task",
+                    "id": "A1",
+                    "state": "ok"
+                },
+                {
+                    "type": "Task",
+                    "id": "A2",
+                    "state": "ok"
+                },
+                {
+                    "type": "LogicalOperator",
+                    "operator": "and",
+                    "elements": [
+                        {
+                            "type": "Task",
+                            "id": "A3",
+                            "state": "ok"
+                        },
+                        {
+                            "type": "Task",
+                            "id": "A4",
+                            "state": "ok"
+                        }
+                    ]
+                }
+            ],
+
+        }
+    },
+    {
+        "id": "A1",
+        "type": "A"
+    },
+    {
+        "id": "A2",
+        "type": "A"
+    },
+    {
+        "id": "A3",
+        "type": "A"
+    },
+    {
+        "id": "A4",
+        "type": "A"
+    }
+]
+export default data
