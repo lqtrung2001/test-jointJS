8d9346dd50ef80e6b0b32649e4e2e80aedf4c2c1
Luong Quoc Trung <lqtrung20122001@gmail.com>
2022-11-08 11:09:08 +0700

    format file App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import svgPanZoom from 'svg-pan-zoom';
 import nextId from "react-id-generator";
 import data from './data.js'
 
+var width = 2000;
+var x = 0;
+
 function makeLink(parentElementID, childElementID) {
     return new joint.shapes.standard.Link({
         source: { id: parentElementID },
@@ -13,22 +16,7 @@ function makeLink(parentElementID, childElementID) {
     });
 }
 
-function flattenJSON(obj) {
-    let res = {}, extraKey = '';
-    for (let key in obj) {
-        if (typeof obj[key] !== 'object') {
-            res[extraKey + key] = obj[key];
-        } else {
-            flattenJSON(obj[key], res, `${extraKey}${key}.`);
-        };
-    };
-    // console.log('res', res);
-    return res;
-}
-var width = 2000
-
 const makeCell = (shape, text, id) => {
-
     return {
         id: id,
         type: shape,
@@ -51,6 +39,7 @@ const makeCell = (shape, text, id) => {
         }
     }
 }
+
 function zoompaper(paper) {
     const panZoom = svgPanZoom(paper.svg, {
 
@@ -99,15 +88,12 @@ function addToCells(jsonData, cells, links) {
                 // console.log("collected: ", deepDive(task.dependencies.elements, "", cells))
                 task.dependencies.id = nextId();
 
-                // width -= 250;
                 // node OR
                 let operCells = makeCell("standard.Circle", task.dependencies.operator, task.dependencies.id)
                 cells.push(operCells)
-                // mapCells.push({operCells, x:0 , y: 1})
+
                 deepDive(task.dependencies.elements, operCells, cells, links);
 
-                // operCells.position.y = 200
-                // operCells.position.x = operCells.position.x + 100
                 let link = makeLink(operCells.id, cell.id)
                 links.push(link);
             }
@@ -120,9 +106,7 @@ function addToCells(jsonData, cells, links) {
 const deepDive = (node, parent, cells, links) => {
     // console.log("node ", node)
     for (let key in node) {
-        // width += 250;
         if (node[key].type === "LogicalOperator") {
-            // width -= 250;
             // collector.push(deepDive(node[key].elements))
             // TODO: Ve hinh tron AND/OR
             // console.log("Draw ", node[key])
@@ -131,35 +115,25 @@ const deepDive = (node, parent, cells, links) => {
             cells.push(cell);
             let link = makeLink(cell.id, parent.id)
             links.push(link);
-            // width -= 250;
             // TODO: noi hinh tron   voi parent
-            // makeLink(parent, this)
             deepDive(node[key].elements, cell, cells, links)
             // Goi lai ham deepDive voi node = node[key].elements va parent = hinh tron
         } else if (node[key].type === "Task") {
-            // width += 250;
             // console.log("task = ", node[key])
-            // collector.push(node[key])
             // TODO: noi task voi parent cua no
             let link = makeLink(node[key].id, parent.id);
             links.push(link);
         }
     }
-    // return collector;
 }
 
-
-var x = 0, y = 0;
-var i = 0;
 function setYforMapCells(mapCells) {
-    console.log('aaaaaaaaaaaaaaa', mapCells.length)
     for (let i = 0; i < mapCells.length; i++) {
         let tem = 1;
         for (let j = i + 1; j < mapCells.length; j++) {
-            
-            if (mapCells[i].y == 0 && mapCells[j].x == mapCells[i].x)
-            {
-                console.log(" mapCells[i].y " + mapCells[i].y + " mapCells[j].x " + mapCells[j].x + " mapCells[i].x " +mapCells[i].x)
+
+            if (mapCells[i].y == 0 && mapCells[j].x == mapCells[i].x) {
+                // console.log(" mapCells[i].y " + mapCells[i].y + " mapCells[j].x " + mapCells[j].x + " mapCells[i].x " +mapCells[i].x)
                 mapCells[j].y = tem++;
             }
         }
@@ -168,45 +142,19 @@ function setYforMapCells(mapCells) {
 
 function addToMapCells(jsonData, mapCells) {
     jsonData.forEach(task => {
-        const cell = makeCell("standard.Rectangle", task.id, task.id);
-        // console.log('data cell',mapCells[0].cell.id)
-        let mapCellIndex = mapCells.findIndex((mapCell => mapCell.cell.id == task.id));
-        let mapCell = mapCells[mapCellIndex];
-        console.log(i++, mapCellIndex);
-        if (mapCell.x == 0 && mapCell.y == 0) {
-            // console.log('map cell index', mapCellIndex);
-            // console.log('map cell', mapCells[mapCellIndex]);
-            // mapCell.x = x;
-            // mapCell.y = y;
-        }
-        // mapCells.push(cell, x, y);
         if (task.dependencies) {
             if (task.dependencies.type === "LogicalOperator") {
-                // task.dependencies.id = nextId();
-                // node OR
-
                 let mapCellIndexOper = mapCells.findIndex((mapCell => mapCell.cell.id == task.dependencies.id));
                 let mapCellOper = mapCells[mapCellIndexOper];
                 // console.log('map cell index Oper', mapCellIndexOper);
-                console.log(i++, mapCellIndexOper);
-                if (mapCellOper.x === 0 && mapCellOper.y === 0) {
-                    x = x + 1;
-                    // console.log('map cell index Oper', mapCellIndexOper);
-                    // console.log('map cell Oper', mapCells[mapCellIndexOper]);
-                    mapCellOper.x = x;
-                    // mapCellOper.y = y;
-
-                }
+                if (mapCellOper.x === 0 && mapCellOper.y === 0) mapCellOper.x = ++x;
                 const operCells = makeCell("standard.Circle", task.dependencies.operator, task.dependencies.id)
-                // mapCells.push({operCells, x:0 , y: 1})
-                // x++;
-                deepDiveMapCells(task.dependencies.elements, operCells, mapCells);
+                deepDiveMapCells(task.dependencies.elements, mapCells);
             }
         }
-
     });
 }
-const deepDiveMapCells = (node, parent, mapCells) => {
+const deepDiveMapCells = (node,  mapCells) => {
     let flag = 0;
     x++;
     for (const key in node) {
@@ -216,51 +164,48 @@ const deepDiveMapCells = (node, parent, mapCells) => {
             flag = 1;
             if (mapCellOper.x === 0 && mapCellOper.y === 0) {
                 mapCellOper.x = x;
-                // mapCellOper.y = y;
-
             }
             const cell = makeCell("standard.Circle", node[key].operator, node[key].id)
-            // mapCells.push(cell);
-            deepDiveMapCells(node[key].elements, cell, mapCells)
+            deepDiveMapCells(node[key].elements, mapCells)
         } else if (node[key].type === "Task") {
             if (flag) {
                 flag = 0;
                 x--;
             }
             mapCellOper.x = x;
-            // mapCellOper.y = y;
         }
-        console.log(i++, mapCellIndexOper);
-        // x = x + 1;
     }
 }
 function setPosition(cells, mapCells) {
     cells.forEach((cell, index) => {
-        cell.position = { x: cell.position.x - mapCells[index].x * 350, y: cell.position.y + mapCells[index].y*250 }
-        // console.log('aaaaaaaaaa', cell.setPosition )
+        cell.position = { x: cell.position.x - mapCells[index].x * 350, y: cell.position.y + mapCells[index].y * 250 }
     })
 }
-
+function putCellsToMapCells (cells, mapCells) {
+    cells.forEach(cell => {
+        mapCells.push({ cell, x: 0, y: 0 });
+    })
+}
 function App() {
     const jsonData = data;
     var namespace = joint.shapes;
     var cells = [];
     var links = [];
     const mapCells = []
+    
 
     addToCells(jsonData, cells, links, mapCells)
-    cells.forEach(cell => {
-        mapCells.push({ cell, x: 0, y: 0 });
-    })
+    putCellsToMapCells(cells, mapCells)
     addToMapCells(jsonData, mapCells)
     setYforMapCells(mapCells)
-    var graph = new joint.dia.Graph({}, { cellNamespace: namespace });
-
     setPosition(cells, mapCells);
-    graph.resetCells([...cells, ...links]);
-
+    
     console.log('data', cells)
     console.log('data map', mapCells)
+
+    var graph = new joint.dia.Graph({}, { cellNamespace: namespace });
+    graph.resetCells([...cells, ...links]);
+
 
     useEffect(() => {
 
@@ -279,7 +224,6 @@ function App() {
     return (
         <div id='myholder'>
             <h1>aaaaaaaaaa</h1>
-
         </div>
     );
 }
